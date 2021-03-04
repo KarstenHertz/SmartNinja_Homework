b2513bd9cc69126926aa867f88b8adcf6a913f24
KarstenHertz <80061518+KarstenHertz@users.noreply.github.com>
2021-03-04 18:53:23 +0100

    Add files via upload

---
+++ Karsten_Hertz_SmartNinja_Python_HomeWork_1.2.py
@@ -0,0 +1,7 @@
+# Homework 1.2 "Guess the secret Number"
+secret = "3"
+guess = input("Guess a number:")
+if guess == secret:
+    print("Congratulation its correct")
+else:
+    print("Wrong, please try again")
---
+++ Karsten_Hertz_SmartNinja_Python_Homework_1_1.py
@@ -0,0 +1,21 @@
+
+# Homework 1.1
+
+user_input = input("Please enter your mood")
+
+if user_input == "Happy":
+    print("It is great to see you")
+elif user_input == "Nervous":
+    print("do your Yoga sequence")
+elif user_input == "Excited":
+    print("visit your girlfriend")
+elif user_input == "Relaxed":
+    print("Good for you")
+else:
+    print("I don't recognize this mood")
+
+
+
+
+
+
